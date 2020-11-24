8893d2e5b97240b5f89b539f7063a7215d963c0c
Wendellsg <wendell.silva.guimaraes@gmail.com>
2020-11-24 18:20:07 -0300

    Template React Limpo

---
+++ src/components/app/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+    return(
+        <div>
+            <h1>Profiteiro Bdo</h1>
+            <h2>Calcule o lucro exato das suas receitas no Black Desert</h2>
+        </div>
+    )
+}
+
+export default App;
