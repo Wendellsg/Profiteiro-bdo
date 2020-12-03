865b6c4497f77cce13feddb5c08573594a7e1124
Wendellsg <wendell.silva.guimaraes@gmail.com>
2020-12-03 09:50:37 -0300

    Banco de dados completo

---
+++ src/Dados/Itens.js
@@ -0,0 +1,14 @@
+const itens= [
+
+    {id: 4987, name:'Cristal Negro de Magia Concentrada', img: 'https://blackdesert-tradeweb.playredfox.com/TradeMarket/Common/img/BDO/item/4987.png'},
+    {id: 5000, name:'cristal Negro', img:'https://blackdesert-tradeweb.playredfox.com/TradeMarket/Common/img/BDO/item/5000.png'},
+    {id: 4998, name:'Fragmento Afiado de Cristal Negro', img:'https://blackdesert-tradeweb.playredfox.com/TradeMarket/Common/img/BDO/item/4998.png'},
+    {id: 4997, name:'Fragmento Duro de Cristal Negro', img:'https://blackdesert-tradeweb.playredfox.com/TradeMarket/Common/img/BDO/item/4997.png'},
+    {id: 16002, name:'Pedra Negra (Armadura)', img:'https://blackdesert-tradeweb.playredfox.com/TradeMarket/Common/img/BDO/item/16002.png'},
+    {id: 16001, name:'Pedra Negra (Arma)', img:'https://blackdesert-tradeweb.playredfox.com/TradeMarket/Common/img/BDO/item/16001.png'},
+
+]
+
+
+
+export default itens;
--- src/pages/receitas/receitas.jsx
+++ src/pages/receitas/receitas.jsx
@@ -1,18 +1,19 @@
 import Button from '../../components/button/Button'
 import {Link} from 'react-router-dom'
 import Item from '../../components/itens/item'
-import Itemimage from '../../assets/00004987.png'
 import './receitas.css';
 import InputText from '../../components/inputText/inputText'
+import Itens from '../../Dados/Itens'
 
 function Receitas(){
     
     return(
         <div className="conteiner" >
-            <h1>Selecione um item</h1>
-            <Item img={Itemimage}name="Cristal Negro de Magia Concentrada"/>
+            <h1>Diga quanto</h1>
+            <Item  img={Itens[0].img} name={Itens[0].name}/>
             
             <InputText placeholder="Quantas unidades você quer crafitar?"/>
+
 
             <Link to="/resultado">
                 <Button texto="Selecionar"></Button>
