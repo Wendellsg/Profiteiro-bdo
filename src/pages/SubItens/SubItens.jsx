2ff8af91b1db54d63f443eaa5c375d9f0853e16f
Wendellsg <wendell.silva.guimaraes@gmail.com>
2020-12-08 18:57:52 -0300

    Feita a pagina de coleta

--- src/pages/SubItens/SubItens.jsx
+++ src/pages/SubItens/SubItens.jsx
@@ -1,26 +1,29 @@
 import Button from '../../components/button/Button'
 import {Link} from 'react-router-dom'
 import Item from '../../components/itens/item'
-import './receitas.css';
+import './SubItens.css';
 import InputText from '../../components/inputText/inputText'
 import Itens from '../../Dados/Itens'
 
-function Receitas(){
-    
+
+
+function SubItens(){
     return(
+
         <div className="conteiner" >
-            <h1>Diga quanto</h1>
+            <h1>Me diga quanto desse item você já possue</h1>
             <Item  img={Itens[0].img} name={Itens[0].name}/>
             
-            <InputText placeholder="Quantas unidades você quer crafitar?"/>
+            <InputText placeholder="Quantas unidades você ja tem?"/>
 
-
+            <Link to="/resultado">
+                <Button texto="Próximo"></Button>
+            </Link>
             <Link to="/subitem?1">
-                <Button texto="Selecionar"></Button>
+                <Button texto="Voltar"></Button>
             </Link>
         </div>
     )
+}
 
-};
-
-export default Receitas;
+export default SubItens
--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ import {
 import Home from './pages/Home/home'
 import Receitas from './pages/receitas/receitas'
 import Resultado from './pages/resultado/resultado'
+import SubItens from './pages/SubItens/SubItens'
 
 
 function Routes(){
@@ -23,6 +24,9 @@ function Routes(){
                 </Route>
                 <Route path="/resultado">
                 <Resultado />
+                </Route>
+                <Route path="/subitem">
+                <SubItens />
                 </Route>
                 </Switch>
             </BrowserRouter>
